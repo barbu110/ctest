26cfb7b1e1d79cedb0a44dc5187d2c9e53923ef1
Alexandru Rusu <aqrusu@gmail.com>
2019-08-08 09:35:57 +0300

    Add ASSERT_STRCMP.

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 CFLAGS := -Iinclude
 
-example: example.c src/ctest_main.o
+example: example.c src/ctest_main.o src/assertions.o
 
 src/ctest_main.o: src/ctest_main.c
+
+src/assertions.o: src/assertions.c
 
 clean:
 	@echo "Cleaning..."
---
+++ src/assertions.c
@@ -0,0 +1,15 @@
+#include "ctest/assertions.h"
+
+int opi(int a, enum iops_enum b, int c)
+{
+  static const char array[] = {
+#define $(x,op)   &&x - &&ADD,
+    IOPS
+#undef $
+  };
+  if (b >= IOPSENUM_COUNT) return a;
+  goto *(&&ADD + array[b]);
+#define $(x,op)   x: return a op c;
+  IOPS
+#undef $
+}
